d361e9674bf94e9e21b1a6563cba476da65b6947
agonzd <agonzd@uc.cl>
2023-06-18 22:38:36 -0400

    Avances componente

--- simple.js
+++ simple.js
@@ -12,20 +12,53 @@ export class SimpleComponent extends LitElement {
   static styles = css`
     :host {
       font-family: Verdana, sans-serif;
-      font-size: 24px;
+      color: #000000;
     }
 
     .badly-rated {
-      background-color: #F55D3E;
+      border: 7.5px solid #F55D3E;
     }
 
     .well-rated { 
-      background-color: #007EA7;
+      border: 7.5px solid #007EA7;
+    }
+
+    .product-card {
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      background-color: #FFFFFF;
+      border-radius: 15px;
+      padding: 10px;
+      margin: 10px;
+    }
+
+    .product-name {
+      font-weight: bold;
+      text-align: center;
+      padding: 10px;
+      max-width: 20ch;
+      height: 5em;
+      font-size: 1.5rem;
+    }
+
+    .product-image {
+      width: 15vmax;
+      min-height: 15vmax;
+      max-height: auto;
+      margin: 10px;
+      padding: 10px;
+    }
+
+    .product-image img {
+      max-width: 100%;
+      width: 20vw;
+      height: 20vw;
     }
 
     .rating {
       color: #F8D23A;
-      font-size: 44px
+      font-size: 2rem;
     }
 
     .price-before {
@@ -34,29 +67,20 @@ export class SimpleComponent extends LitElement {
     }
     
     .price-after {
-      font-size: 44px;
       font-weight: bold;
-    }
-
-    .product-card { 
-      display: flex;
-      flex-direction: column;
-      align-items: center;
-      border-radius: 10px;
-      border: 3px solid #FFFFFF;
+      font-size: 2.5rem;
     }
 
     .product-info {
       display: flex;
-      justify-content: space-evenly;
       align-items: center;
+      font-size: 1.5rem;
     }
 
     .discount-box {
-      font-size: 34px;
       font-weight: bold;
       border-radius: 20px;
-      border: 5px solid #FFFFFF;
+      border: 5px solid #000000;
       background-color: #F8D23A;
       padding: 5px;
       margin-left: 50px;
@@ -68,30 +92,21 @@ export class SimpleComponent extends LitElement {
     this.rating = 7;
     this.discount = 0;
     this.name = 'Product Name';
-    this.image = 'https://via.placeholder.com/150';
+    this.image = 'https://via.placeholder.com/250';
     this.price = '29990';
   }
   
-  cardTemplate(ratingTem, discountTem) { 
+  cardTemplate(ratingTem, priceTem) { 
     const color = this.rating >= 5 ? 'well-rated' : 'badly-rated';
-    const finalPrice = Math.round(+this.price - (+this.price * +this.discount / 100));
-    const priceSection = html`
-      ${this.discount > 0 ? html`
-        <div class="product-info">
-          <p class="price-before">$${this.price}</p>
-          ${discountTem}
-        </div>` : ''}
-      <p class="price-after">$${finalPrice}</p>
-    `;
 
     return html`
       <div class="product-card ${color}">
-        <p>${this.name}</p>
+        <p class="product-name">${this.name}</p>
         <div class="product-image">
           <img src="${this.image}" alt="${this.name}">
         </div>
         ${ratingTem}
-        ${priceSection}
+        ${priceTem}
       </div>
     `
   }
@@ -105,13 +120,21 @@ export class SimpleComponent extends LitElement {
     return html`<p class="rating">${makeStars(this.rating)}</p>`;
   }
 
-  discountTemplate() {
-    return html`<p class="discount-box">${this.discount}%</p>`;
+  priceTemplate() { 
+    const finalPrice = Math.round(+this.price - (+this.price * +this.discount / 100));
+    return html`
+      ${this.discount > 0 ? html`
+        <div class="product-info">
+          <p class="price-before">$${this.price}</p>
+          <p class="discount-box">${this.discount}%</p>
+        </div>` : ''}
+      <p class="price-after">$${finalPrice}</p>
+    `;
   }
 
   render() {
     return html`
-      ${this.cardTemplate(this.ratingTemplate(), this.discountTemplate())}
+      ${this.cardTemplate(this.ratingTemplate(), this.priceTemplate())}
     `;
   }
 }
